c38f576ffde42de97227864823b5502fb8e6b6eb
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-20 16:48:31 +0700

    conditionally rendering & working with modal/backdrop props

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,9 @@
-const Modal = () => {
+const Modal = (props) => {
   return (
     <div className="modal">
       <h2>Are you sure?</h2>
       <div className="actions">
-        <button className="btn btn--alt">Cancel</button>
+        <button className="btn btn--alt" onClick={props.onClick}>Cancel</button>
         <button className="btn">Confirm</button>
       </div>
     </div>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
+import Backdrop from "./Backdrop";
+import Modal from "./Modal";
+
 const Todo = (props) => {
+  const [showModal, setShowModal] = useState(false);
   const deleteHandler = () => {
-      console.log('Click')
+    setShowModal(true);
+  };
+  const closeModalHandler = () => {
+    setShowModal(false)
   }
   return (
     <li className="todo-item">
       <h2>{props.text}</h2>
-      <button className="btn" onClick={deleteHandler}>Delete</button>
+      <button className="btn" onClick={deleteHandler}>
+        Delete
+      </button>
+      {showModal && <Modal onClick={closeModalHandler} />}
+      {showModal && <Backdrop onClick={closeModalHandler} />}
     </li>
   );
 };
