1259801002bbb84aeb363fab9f125d59d9617a9a
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-20 13:28:32 +0700

    todo ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 function App() {
-  return <div>Hello!</div>;
+  return <div className="todo">
+    <header className="header">Todo App</header>
+    <div className="card">
+      <input type="text" className="add-todo" placeholder="Enter title"/>
+      <button className="btn">Add</button>
+    </div>
+    <ul className="todo-list">
+      <li className="todo-item">
+        <h2>Title</h2>
+        <button className="btn">Delete</button>
+      </li>
+      <li className="todo-item">
+        <h2>Title</h2>
+        <button className="btn">Delete</button>
+      </li>
+      <li className="todo-item">
+        <h2>Title</h2>
+        <button className="btn">Delete</button>
+      </li>
+    </ul>
+  </div>;     
 }
 
 export default App;
